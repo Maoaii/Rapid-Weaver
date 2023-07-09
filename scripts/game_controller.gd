bbeff4fe19bf95133a0f494310978b463317d8e1
Girotto <l.girottoph@gmail.com>
2023-07-10 00:41:11 +0100

    player movement done

--- scripts/game_controller.gd
+++ scripts/game_controller.gd
@@ -1,7 +1,6 @@
 extends Node2D
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_ddelta):
+func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
