b0a2ab27a80b4dd5ab115e7c878993d5df6f74b5
Girotto <l.girottoph@gmail.com>
2024-01-04 22:29:38 +0000

    destructable platform poofs when desintegrated

--- RapidWeaver/scripts/obstacles/destructable_platform.gd
+++ RapidWeaver/scripts/obstacles/destructable_platform.gd
@@ -7,14 +7,21 @@ signal _on_destroyed()
 
 @onready var destroy_timer: Timer = $DestroyTimer
 @onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var poof_particles: CPUParticles2D = $PoofParticles
+@onready var touch_detector: Area2D = $TouchDetector
+var destroyed: bool = false
 
 func _ready() -> void:
 	destroy_timer.wait_time = destroy_time
 	animated_sprite.play("100")
 
 func _process(_delta: float) -> void:
+	if destroyed and poof_particles.emitting == false:
+		queue_free()
+	
 	if destroy_timer.is_stopped():
 		return
+	
 	
 	if destroy_timer.time_left <= destroy_time / 4:
 		animated_sprite.play("25")
@@ -29,7 +36,11 @@ func start_timer() -> void:
 
 func _on_destroy_timer_timeout():
 	emit_signal("_on_destroyed")
-	queue_free()
+	poof_particles.restart()
+	animated_sprite.visible = false
+	$CollisionShape2D.set_deferred("disabled", true)
+	touch_detector.set_deferred("monitoring", false)
+	destroyed = true
 
 func _on_touch_detector_body_entered(body):
 	if body.is_in_group("Player"):
