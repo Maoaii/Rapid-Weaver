3e65e22308ebec96505b9359f97869ff1c9fdfa4
Girotto <l.girottoph@gmail.com>
2024-01-06 22:26:24 +0000

    cleaned up code

--- RapidWeaver/scripts/managers/bg_music_manager.gd
+++ RapidWeaver/scripts/managers/bg_music_manager.gd
@@ -1,9 +1,16 @@
 class_name BGMusicManager
 extends Node2D
 
-var bg_music: AudioStreamPlayer2D
+## Master bus for audio playing
+@onready var master_bus = AudioServer.get_bus_index("Master")
+## Music bus for audio playing
+@onready var music_bus = AudioServer.get_bus_index("Music")
+## SFX bus for audio playing
+@onready var sfx_bus = AudioServer.get_bus_index("SFX")
 
-var type_to_music: Dictionary = {
+## Dictionary that holds information about all the music available.
+## Doubles down as a type of music to asset converter
+const type_to_music: Dictionary = {
 	"menu": {
 		"asset": preload("res://assets/sound/music/pixel_perfect.mp3"),
 		"volume": 0
@@ -13,27 +20,33 @@ var type_to_music: Dictionary = {
 		"volume": -20
 	}
 }
-var last_music_pos: float = 0.0
 
-@onready var master_bus = AudioServer.get_bus_index("Master")
-@onready var music_bus = AudioServer.get_bus_index("Music")
-@onready var sfx_bus = AudioServer.get_bus_index("SFX")
+## Audio Player for background music
+var bg_music: AudioStreamPlayer2D
+func set_bus_volumes(master_bus_volume: float, music_bus_volume: float, sfx_bus_volume: float) -> void:
+	AudioServer.set_bus_volume_db(master_bus, master_bus_volume)
+	AudioServer.set_bus_volume_db(music_bus, music_bus_volume)
+	AudioServer.set_bus_volume_db(sfx_bus, sfx_bus_volume)
 
-func _ready() -> void:
-	AudioServer.set_bus_volume_db(master_bus, linear_to_db(SaveGame.load_data("Master Volume").get("Master Volume")))
-	AudioServer.set_bus_volume_db(music_bus, linear_to_db(SaveGame.load_data("Music Volume").get("Music Volume")))
-	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(SaveGame.load_data("SFX Volume").get("SFX Volume")))
-	
+func setup_audio_player() -> void:
 	bg_music = AudioStreamPlayer2D.new()
 	bg_music.bus = "Music"
+	bg_music.process_mode = Node.PROCESS_MODE_ALWAYS
+	
 	await get_tree().root.ready
+	
 	get_tree().root.add_child(bg_music)
+
+func _ready() -> void:
+	set_bus_volumes(linear_to_db(SaveGame.load_data("Master Volume").get("Master Volume")), \
+					linear_to_db(SaveGame.load_data("Music Volume").get("Music Volume")), \
+					linear_to_db(SaveGame.load_data("SFX Volume").get("SFX Volume")))
+	
+	setup_audio_player()
 	
 	play_music("menu")
-	bg_music.process_mode = Node.PROCESS_MODE_ALWAYS
 
 func play_music(type: String, at_pos: float = 0.0) -> void:
-	last_music_pos = bg_music.get_playback_position()
 	if type_to_music.get(type) == null:
 		return
 	
