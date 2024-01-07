e462196d176e75cb244b3110866dc67e5767f394
Girotto <l.girottoph@gmail.com>
2024-01-07 22:48:11 +0000

    quit popup added

--- RapidWeaver/scripts/menus/quit_area.gd
+++ RapidWeaver/scripts/menus/quit_area.gd
@@ -3,4 +3,12 @@ extends Area2D
 
 func _on_area_entered(area):
 	if area.is_in_group("Hook"):
-		get_tree().quit()
+		$PopupPanel.show()
+
+
+func _on_no_pressed():
+	$PopupPanel.hide()
+
+
+func _on_yes_pressed():
+	get_tree().quit()
