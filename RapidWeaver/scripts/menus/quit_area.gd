d4a15bb4b6e4b506b5099100d4b314ccb9c59f86
Girotto <l.girottoph@gmail.com>
2024-01-09 19:24:02 +0000

    new layout

--- RapidWeaver/scripts/menus/quit_area.gd
+++ RapidWeaver/scripts/menus/quit_area.gd
@@ -2,7 +2,7 @@ extends Area2D
 
 
 func _on_area_entered(area):
-	if area.is_in_group("Hook"):
+	if area.is_in_group("Player"):
 		$PopupPanel.show()
 		get_tree().paused = true
 
@@ -16,3 +16,9 @@ func _on_yes_pressed():
 
 func _on_popup_panel_popup_hide():
 	get_tree().paused = false
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		$PopupPanel.show()
+		get_tree().paused = true
--- RapidWeaver/scripts/menus/settings_area.gd
+++ RapidWeaver/scripts/menus/settings_area.gd
@@ -2,10 +2,16 @@ extends Area2D
 
 
 func _on_area_entered(area):
-	if area.is_in_group("Hook"):
+	if area.is_in_group("Player"):
 		$Settings.visible = true
 		get_tree().paused = true
 
 
 func _on_settings_popup_hide():
 	get_tree().paused = false
+
+
+func _on_body_entered(body):
+	if body.is_in_group("Player"):
+		$Settings.visible = true
+		get_tree().paused = true
